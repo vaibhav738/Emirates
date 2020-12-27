460bdf888ef67d9d5a8e16a17764669d89f93480
A <A@abc>
2020-12-26 21:45:56 -0800

    [vaibhav]: As i host i created the time schedule

---
+++ src/Airlines/FlightTiming.java
@@ -0,0 +1,16 @@
+package Airlines;
+
+public class FlightTiming {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcomes to Emirates Airline"); 
+		
+		
+		
+	}
+	
+	
+	
+}
